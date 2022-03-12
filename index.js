85b138b3bf637f0853c0f436bd5878fb5966131e
brico1994 <b.rico.barrilero@gmail.com>
2022-03-12 16:57:36 +0100

    public html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.arguments("/",express.static('public'))
+app.use("/",express.static('public'))
 
 app.get("/faces",(req,res)=>{
     console.log("requested / route")

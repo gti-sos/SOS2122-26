26cc3cf675a598b69b80b920b180170007e84d9b
pabgalace <pablogaac@gmail.com>
2022-03-21 13:54:01 +0100

    Deploy

---
+++ Desktop/Nopcional/SOS2122-26-65ed73668d32d5e331f139a1ee5462e6f452fd92/index.js
@@ -0,0 +1,17 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use("/",express.static('public'))
+
+app.get("/cool",(req,res)=>{
+    console.log("requested / route")
+    res.send("<html><body><h1>"+cool()+"</h1></body></html>")
+});
+
+app.listen(port,()=>{
+    console.log(`Server TRULY ready at port ${port}`);
+}); 
+console.log(`Servidor listo ${port}`);

5eab7e3d7390f10ee176ad60aae8ceab79d19052
manueljgb <manueljglezb@gmail.com>
2022-04-25 12:56:21 +0200

    Info

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Router from "svelte-spa-router";
 	import Home from "./Home.svelte";
+	import Info from "./Info.svelte";
 	import Defense from "./defense-spent-stats/Defense.svelte";
 	import DefenseEdit from "./defense-spent-stats/DefenseEdit.svelte";
 	import Electricity from "./electricity-generation-stats/Electricity.svelte";
@@ -12,7 +13,8 @@
 		"/defense-spent-stats" : Defense,
 		"/defense-spent-stats/:country/:year" : DefenseEdit,
 		"/electricity-generation-stats" : Electricity,
-		"/electricity-generation-stats/:country/:year" : ElectricityEdit
+		"/electricity-generation-stats/:country/:year" : ElectricityEdit,
+		"/info": Info
 	};
 </script>
 

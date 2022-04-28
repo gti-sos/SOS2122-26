9ecd78f19280a968504b0e03c12032eed18b3651
pabgalace <pablogaac@gmail.com>
2022-04-28 11:45:57 +0200

    fix editDefense

--- src/front/defense-spent-stats/DefenseEdit.svelte
+++ src/front/defense-spent-stats/DefenseEdit.svelte
@@ -114,3 +114,27 @@
 
     <Button outline color="secondary" on:click="{pop}">Atrás</Button>
 </main>
+
+<style>
+
+    input{
+        width: 100%;
+    }
+    
+    thead{
+        background-color: #246355;
+        border-bottom: solid 5px #0F362D;
+        color: white;
+    }
+    
+    tr:nth-child(even){
+        background-color: #ddd;
+    }
+    
+    tr:hover td{
+        background-color: #369681;
+        color: white;
+    }
+    
+    
+    </style>

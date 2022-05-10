9eea257fbae98f0491a65009ba08317e5a073ae8
pabgalace <pablogaac@gmail.com>
2022-05-10 12:05:42 +0200

    Prueba graph2

--- src/front/CommonGraph.svelte
+++ src/front/CommonGraph.svelte
@@ -62,8 +62,6 @@ async function loadGraph(){
                 }
             });
         }
-        console.log("vamos por aqui....");
-
         if(defenseChartInfo.length == 0 && electricityChartInfo.length == 0){
             console.log("ERROR MSG");
             alert("Por favor primero cargue los datos en al menos una de las APIs");
@@ -75,7 +73,7 @@ async function loadGraph(){
         console.log("Generando datos...");
         Highcharts.chart('container', {
             chart: {
-                type: 'line'
+                type: 'lollipop'
             },
             title: {
                 text: 'Gráfica conjunta por Países'
@@ -98,21 +96,7 @@ async function loadGraph(){
             }, {
                 name: 'Variación en Consumo de Electricidad',
                 data: electricityChartVar
-            }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
-                    },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
-                        }
-                    }
-                }]
-            }
+            }]
         });
 
     }
@@ -120,13 +104,17 @@ async function loadGraph(){
 </script>
 
 <svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"  on:load="{loadGraph}"></script>    
-</svelte:head>
 
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <script src="https://code.highcharts.com/modules/series-label.js"></script>
+  <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  <script src="https://code.highcharts.com/highcharts-more.js"></script>
+  <script src="https://code.highcharts.com/modules/export-data.js"></script>
+  <script src="https://code.highcharts.com/modules/dumbbell.js"></script>
+  <script src="https://code.highcharts.com/modules/lollipop.js"></script>
+  <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+
+</svelte:head>
 
 
 <main>
@@ -141,3 +129,42 @@ async function loadGraph(){
   <p align="center"><Button outline color="secondary" on:click="{pop}">Atrás</Button></p>
  
 </main>
+
+<style>
+        .highcharts-figure, .highcharts-data-table table {
+            min-width: 320px; 
+            max-width: 800px;
+            margin: 1em auto;
+        }
+        #container {
+            height: 450px;
+        }
+        .highcharts-data-table table {
+            font-family: Verdana, sans-serif;
+            border-collapse: collapse;
+            border: 1px solid #EBEBEB;
+            margin: 10px auto;
+            text-align: center;
+            width: 100%;
+            max-width: 500px;
+        }
+        .highcharts-data-table caption {
+            padding: 1em 0;
+            font-size: 1.2em;
+            color: #555;
+        }
+        .highcharts-data-table th {
+            font-weight: 600;
+            padding: 0.5em;
+        }
+        .highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+            padding: 0.5em;
+        }
+        .highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+            background: #f8f8f8;
+        }
+        .highcharts-data-table tr:hover {
+            background: #f1f7ff;
+        }
+  
+  </style>

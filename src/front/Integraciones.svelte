f31a8d850951df41271de340aeb8f4eb1038f180
pabgalace <pablogaac@gmail.com>
2022-05-29 13:58:02 +0200

    Hot fix

--- src/front/Integraciones.svelte
+++ src/front/Integraciones.svelte
@@ -12,22 +12,22 @@
             </h2>
             
             <p>
-                <a href="https://sos2122-26.herokuapp.com/defense-spent-stats/int-1">Integración 1</a>(Integración Widget <a href="https://sos2122-27.herokuapp.com/api/v2/public-debt-stats">public-debt-stats</a>)
+                <a href="https://sos2122-26.herokuapp.com/#/defense-spent-stats/int-1">Integración 1</a>(Integración Widget <a href="https://sos2122-27.herokuapp.com/api/v2/public-debt-stats">public-debt-stats</a>)
             </p>
             <p> 
-                <a href="https://sos2122-26.herokuapp.com/defense-spent-stats/int-2">Integración 2</a>(Integración Widget <a href="http://sos2122-10.herokuapp.com/api/v2/population-levels">population-levels</a>)
+                <a href="https://sos2122-26.herokuapp.com/#/defense-spent-stats/int-2">Integración 2</a>(Integración Widget <a href="http://sos2122-10.herokuapp.com/api/v2/population-levels">population-levels</a>)
             </p>
             <p> 
-                <a href="https://sos2122-26.herokuapp.com/defense-spent-stats/int-3">Integración 3</a>(Widget Api Ext Poblacion)
+                <a href="https://sos2122-26.herokuapp.com/#/defense-spent-stats/int-3">Integración 3</a>(Widget Api Ext Poblacion)
             </p>
             <p> 
-                <a href="https://sos2122-26.herokuapp.com/defense-spent-stats/int-4">Integración 4</a>(Widget Api Ext Covid-19)
+                <a href="https://sos2122-26.herokuapp.com/#/defense-spent-stats/int-4">Integración 4</a>(Widget Api Ext Covid-19)
             </p>
             <p> 
-                <a href="https://sos2122-26.herokuapp.com/defense-spent-stats/int-5">Integración 5</a>(Widget Api Ext HearthStone)
+                <a href="https://sos2122-26.herokuapp.com/#/defense-spent-stats/int-5">Integración 5</a>(Widget Api Ext HearthStone)
             </p>
             <p> 
-                <a href="https://sos2122-26.herokuapp.com/defense-spent-stats/int-6">Integración 6</a> (Widget Api Ext Precipitaciones)
+                <a href="https://sos2122-26.herokuapp.com/#/defense-spent-stats/int-6">Integración 6</a> (Widget Api Ext Precipitaciones)
             </p>
             
         </div>

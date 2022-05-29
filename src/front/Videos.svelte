fb0aadaa8a7c55c69f2d2ad8e29dd8949f0ba9e0
manueljgb <manueljglezb@gmail.com>
2022-05-30 00:16:56 +0200

    video manu

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -11,17 +11,25 @@
                 Vídeo Pablo:
             </h2>
             
-            <p>
+            <!-- <p>
                 <a href="https://www.youtube.com/watch?v=HjFXXxQfChY">Video Explicativo Pablo</a>
-            </p>
-            
+            </p> -->
+            <div>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/HjFXXxQfChY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
             
         </div>
         <div>
-
+            
             <h2 >
                 Vídeo Manuel:
             </h2>
+            <div style="padding:75% 0 0 0;position:relative;">
+                <iframe src="https://player.vimeo.com/video/715027373?h=3ebfadce95&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+                frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="SOS.mp4">
+                </iframe>
+            </div><script src="https://player.vimeo.com/api/player.js"></script>
 
         </div>
 
